a333103f3bb72668f9b388a24a397e3a22521aa5
Dasari Bhovan <dasaribhovan@gmail.com>
2022-12-19 13:23:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from re import X
 import pandas as pd
 from telegram.ext import *
 x={
-  "API_KEY": "5751319983:AAHiDMjsHUx5SSTV_r4m0yKErPJSYcOobyo"
+  "API_KEY": "5751319983:AAHd3ParxIDqRBcftbbHkgteUBmeP3Y1V9M"
 }
 my_secret = x['API_KEY']
 
